c9ac2c716c54d06076ab027b5c749b7393fa8275
notomo <notomo.motono@gmail.com>
2023-06-01 21:16:14 +0900

    Fix (buffer lib)

--- lua/unionbuf/lib/buffer.lua
+++ lua/unionbuf/lib/buffer.lua
@@ -1,12 +1,18 @@
 local M = {}
 
 function M.has_content(bufnr)
-  local char = vim.api.nvim_buf_get_text(bufnr, 0, 0, 0, 1, {})[1]
-  if not char then
-    -- for example unloaded buffer
-    return false
+  local count = vim.api.nvim_buf_line_count(bufnr)
+  if count > 1 then
+    return true
   end
-  return char ~= ""
+
+  local char = vim.api.nvim_buf_get_text(bufnr, 0, 0, 0, 1, {})[1]
+  if char and char ~= "" then
+    return true
+  end
+
+  -- for example unloaded buffer
+  return false
 end
 
 return M
